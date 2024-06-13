7a59ed46601e769d758a97411834fd338656845c
suchi <suchi@SUCHITRA>
2024-06-13 10:22:36 +0530

    on poll test is excuted

---
+++ DemoJenkinspractice/src/test/java/jenkins/OnPoll_Test.java
@@ -0,0 +1,11 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class OnPoll_Test {
+	@Test
+	public void test() {
+		Reporter.log("on poll test case executed", true);
+	}
+}
